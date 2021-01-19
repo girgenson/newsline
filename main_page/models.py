3b2b437c2e9142496e7ba9f6ae942424d1ba2111
girgenson <vadimgirgenson@yandex.ru>
2021-01-19 17:11:13 +0300

    Mockup of the main page and models created

--- main_page/models.py
+++ main_page/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class PieceOfNews(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+
+
--- main_page/views.py
+++ main_page/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from . models import PieceOfNews
 
 
 def index(request):
-    return render(request, 'index.html')
+    news = PieceOfNews.objects.all()
+    context = {'news': news}
+    return render(request, 'index.html', context)
+
+    class Meta:
+        verbose_name_plural = 'piece of news'
